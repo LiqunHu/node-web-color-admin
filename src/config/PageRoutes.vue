d0b0be2e50136c2dcb276e12b129f76eec7e383c
huliqun <huliquns@126.com>
2018-12-14 14:47:14 +0800

    fix

--- src/config/PageRoutes.vue
+++ src/config/PageRoutes.vue
@@ -6,7 +6,8 @@ const routes = [
   { path: '/error404', component: resolve => require(['@/components/error404'], resolve) },
   { path: '/dashboard/home', component: resolve => require(['@/views/Home'], resolve) },
   { path: '/common/system/SystemApiControl', component: resolve => require(['@/views/common/system/SystemApiControl'], resolve) },
-  { path: '/common/system/GroupControl', component: resolve => require(['@/views/common/system/GroupControl'], resolve) }
+  { path: '/common/system/GroupControl', component: resolve => require(['@/views/common/system/GroupControl'], resolve) },
+  { path: '/common/system/OperatorControl', component: resolve => require(['@/views/common/system/OperatorControl'], resolve) }
 ]
 
 export default routes
---
+++ src/views/common/system/OperatorControl.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <!-- begin breadcrumb -->
+    <ol class="breadcrumb pull-right">
+      <li class="breadcrumb-item active">
+        <a href="javascript:;">系统管理</a>
+      </li>
+    </ol>
+    <!-- end breadcrumb -->
+    <!-- begin page-header -->
+    <h1 class="page-header">
+      员工维护
+      <small></small>
+    </h1>
+    <!-- end page-header -->
+    <panel title="维护">
+      <template slot="beforeBody">
+        <div class="panel-toolbar">
+          <div class="form-inline">
+            <div class="input-group m-r-10">
+              <input type="text" placeholder="搜索用户名、姓名、电话" class="form-control">
+              <div class="input-group-append">
+                <button type="button" class="btn btn-info">
+                  <i class="fa fa-search"></i>
+                </button>
+              </div>
+            </div>
+            <div class="form-group m-r-10">
+              <button type="button" class="btn btn-info">增加员工</button>
+            </div>
+          </div>
+        </div>
+      </template>
+			<Table stripe ref="userTable" :columns="table.userTable.rows" :data="table.iconTable.data"></Table>
+    </panel>
+  </div>
+</template>
+<script>
+import PageOptions from '../../../config/PageOptions.vue'
+
+export default {
+  name: 'OperatorControl',
+  data: function() {
+    return {
+      modal: {},
+      table: {
+        userTable: {
+          rows: [
+            {
+              type: 'index',
+              width: 60,
+              align: 'center'
+            },
+            {
+              title: '用户名',
+              key: 'user_username'
+            },
+            {
+              title: '姓名',
+              key: 'user_name'
+            },
+            {
+              title: '电话',
+              key: 'user_phone'
+            },
+            {
+              title: '邮箱',
+              key: 'user_email'
+            },
+            {
+              title: '用户组',
+              key: 'user_groups'
+            }
+          ],
+          data: []
+        }
+      },
+      formRule: {
+        ruleFolderModal: {
+          systemmenu_name: [{ required: true, trigger: 'change' }],
+          systemmenu_icon: [{ required: true, trigger: 'change' }]
+        }
+      }
+    }
+  },
+  created() {
+    PageOptions.pageEmpty = false
+  }
+}
+</script>
